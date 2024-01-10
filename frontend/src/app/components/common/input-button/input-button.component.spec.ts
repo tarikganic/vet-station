c2d8be78f17b67d9e4b1f4a5089de23528b48031
Ibro02 <ibrahimhodzic2002@gmail.com>
2024-01-10 01:52:09 +0100

    filter dropdown desing

---
+++ frontend/src/app/components/common/input-button/input-button.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InputButtonComponent } from './input-button.component';
+
+describe('InputButtonComponent', () => {
+  let component: InputButtonComponent;
+  let fixture: ComponentFixture<InputButtonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [InputButtonComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(InputButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- frontend/src/app/components/common/input/input.component.ts
+++ frontend/src/app/components/common/input/input.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {faFilter} from "@fortawesome/free-solid-svg-icons";
+import {FormsModule} from "@angular/forms";
+import {InputButtonComponent} from "../input-button/input-button.component";
+import {NgForOf, NgIf} from "@angular/common";
+
 
 @Component({
   selector: 'app-input',
   standalone: true,
   imports: [
-    FaIconComponent
+    FaIconComponent,
+    FormsModule,
+    InputButtonComponent,
+    NgForOf,
+    NgIf
   ],
   templateUrl: './input.component.html',
   styleUrl: './input.component.css'
@@ -14,4 +22,67 @@ import {faFilter} from "@fortawesome/free-solid-svg-icons";
 export class InputComponent {
 
   protected readonly faFilter = faFilter;
+
+  dropdown =
+    [
+      {
+        "key": 0,
+        "title":"Location",
+  "children":[
+{
+  'key': 0,
+  'name': 'Closest Station',
+  'value': false,
+}
+    ]
+      },
+      {
+        "key": 1,
+        "title":"Service type",
+        "children":[
+          {
+            'key': 0,
+            'name': 'In Office',
+            'value': false,
+          },
+          {
+            'key': 1,
+            'name':'On Field',
+            'value': false,
+          }
+
+        ]
+      },
+      {
+        "key": 2,
+        "title":"Accommodation",
+        "children":[
+          {
+            'key': 0,
+            'name': 'Parking',
+            'value': false,
+          },
+          {
+            'key': 1,
+            'name':'Wheelchair',
+            'value': false,
+          },
+          {
+            'key': 2,
+            'name':'Wi-fi',
+            'value': false,
+          }
+
+        ]
+      }
+    ]
+
+
+  filterIsClicked: boolean = false;
+
+  handleChange(temp: boolean, i:number, j:number) {
+    temp = !temp;
+    this.dropdown[i].children[j].value = temp;
+console.log(this.dropdown[i].children[j])
+  }
 }
